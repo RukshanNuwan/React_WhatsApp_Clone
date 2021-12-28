70b0c946a8869e6a7c5e8a64af6a68686145cac6
RukshanNuwan <rukshannuwanbit@gmail.com>
2021-12-28 18:45:31 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB99ar4xSnUDkyZwtYhqryB-pXf6tEr3fA",
+  authDomain: "whatsapp-clone-9cf64.firebaseapp.com",
+  projectId: "whatsapp-clone-9cf64",
+  storageBucket: "whatsapp-clone-9cf64.appspot.com",
+  messagingSenderId: "446042920223",
+  appId: "1:446042920223:web:cc65c7c5f83b74dc030c2a"
+};
